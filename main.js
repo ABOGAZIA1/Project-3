bffca53069e38294e2f9e7cdda08c75fea1e682f
ABOGAZIA <aa7882784@gmail.com>
2024-06-15 17:57:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.getElementById("img")
+let container = document.getElementById("container")
+function phones(phones){
+    img.src= phones
+
+}
+function color(color){
+    container.style.background=color
+}
